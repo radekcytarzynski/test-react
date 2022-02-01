95e8df50ba5f3600534d8b1e8df7fad9ac3173ae
radekcytarzynski <cytarzynskiradoslaw@gmail.com>
2022-02-01 16:52:40 +0100

    Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState(2);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>{number}</div>
+      <button
+        onClick={() => setNumber(number-1)}
+      >-</button>
+      <button
+        onClick={() => setNumber(number+1)}
+      >+</button>
     </div>
-  );
+  )
 }
 
 export default App;
